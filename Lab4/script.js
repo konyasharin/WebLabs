30619b0c3e8afec9423fe5790d1130178db3f740
konyasharin <kolechka.malyshev.04@gmail.com>
2024-02-04 19:04:44 +0700

    finished 6 lab

--- Lab4/script.js
+++ Lab4/script.js
@@ -9,4 +9,32 @@ function init(){
       img.height = img.width
     }
   })
+  // Используем функции для 6 задания
+  console.log(sumArr([10, -3, 23, 1, 1.3]))
+  console.log(minArr([10, -3, 23, 1, 1.3]))
+  console.log(searchSecondBigNumber([10, -3, 23, 1, 1.3]))
+}
+
+// Функции для 6 задания
+// 1)
+function sumArr(arrayOfNums){
+  let sum = 0
+  arrayOfNums.forEach(num => {
+    sum += num
+  })
+  return sum
+}
+
+// 2)
+function minArr(arrayOfNums){
+  let min = 0
+  arrayOfNums.forEach((num, i) => {
+    min = i === 0 ? num : Math.min(num, min)
+  })
+  return min
+}
+
+// 3)
+function searchSecondBigNumber(arrayOfNums){
+  return arrayOfNums.sort()[arrayOfNums.length - 2]
 }
